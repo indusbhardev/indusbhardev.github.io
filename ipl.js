30481b99d4ce5adda9ecc9fe619c96fb4a73a3b9
indusbhardev <abdheshdhakad07@gmail.com>
2026-03-21 17:03:47 +0530

    new

--- ipl.js
+++ ipl.js
@@ -2,18 +2,13 @@ const channels = [
   { 
     name: "STAR SPORTS 1 HINDI", 
     logo: "https://skynet.in.net/stareegr/2019/12/Untitled-design-30__1547961984_114.29.227.0-1.jpg", 
-    url: "https://jiotvmblive.cdn.jio.com//bpk-tv/Star_Sports_1_Hindi_BTS/output/index.mpd?name=Star_Sports_1_Hindi&keyId=8ea9b607549252068e260a5f4d7dd321&key=350c0aaffedf51cc1502e9543c7c885e&cookie=__hdnea__=st=1774087228~exp=1774173628~acl=/*~hmac=f728dd31ae56b2f87c378944778efc90ead70df6e8fc1e63b97211ea9e85cfc6", 
-    keyId: "", 
-    key: "", 
-    cookie: ""
+    url: "https://dash.vodep39240327.workers.dev/?url=https://jiotvmblive.cdn.jio.com//bpk-tv/Star_Sports_1_Hindi_BTS/output/index.mpd?name=Star_Sports_1_Hindi&keyId=8ea9b607549252068e260a5f4d7dd321&key=350c0aaffedf51cc1502e9543c7c885e&cookie=__hdnea__=st=1774087228~exp=1774173628~acl=/*~hmac=f728dd31ae56b2f87c378944778efc90ead70df6e8fc1e63b97211ea9e85cfc6" 
+    
   },
   {  
     name: "STAR ENGLISH", 
     logo: "https://wpleague.in/wp-content/uploads/2026/01/Star-Sports-Network-5.jpeg", 
-    url: "https://example.com/stareng.mp4", 
-    keyId: "jhgd5f6", 
-    key: "jhggh786", 
-    cookie: "jshjgh7.hu8h"
+    url: "https://example.com/stareng.mp4"
   }
 ];
 
@@ -22,7 +17,7 @@ const grid = document.getElementById('star-sports-grid');
 
 channels.forEach(ch => {
   // Construct the long URL
-  const destination = `https://indusbhar.blogspot.com/?url=${ch.url}&name=${encodeURIComponent(ch.name)}&keyid=${ch.keyId}&key=${ch.key}&cookie=${ch.cookie}`;
+  const destination = `https://indusbhar.blogspot.com/?url=${ch.url}`;
 
   // Create the HTML structure
   const card = document.createElement('a');
